d0eac78829fcdacdcea153a071a9b5a40bea56a4
Yoseph Student <blsck1024@gmail.com>
2019-12-02 16:21:37 -0600

    added yearofbirth

--- drillone.js
+++ drillone.js
@@ -1,9 +1,15 @@
-function createGreeting(name, age){   
+function createGreeting(name, age){  
+    const yearOfBirth = 2019 - age;
     let name = "Yoseph"
     let age = 22 
     
-    return 'Hi, my name is ${name} and I am ${age} years old';
+    return 'Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}';
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1)
+
+function createGreeting(name, age){ 
+    const yearOf..........16 - age;
+    return ...
+  }
