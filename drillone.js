7dc080e6c2f0e1fe4f433c8d92367da81053fdab
Taylor Piccarreto <tpic12@gmail.com>
2019-12-02 17:26:19 -0500

    added yob function

--- drillone.js
+++ drillone.js
@@ -1,15 +1,16 @@
-function createGreeting(name, age){  
-    const yearOfBirth = 2019 - age;
-    let name = "Yoseph"
-    let age = 22 
+function createGreeting(name, age){ 
+    let name = "Yoseph";
+    let age = 22;
     
-    return 'Hi, my name is ${name} and I am ${age} years old. I was born in ${yearOfBirth}';
+    
+    return 'Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}';
+}
+
+function getYearOfBirth(age){
+    const yob = 2019-age;
+    return yob;
 }
 
 const greeting1 = createGreeting();
 console.log(greeting1)
 
-function createGreeting(name, age){ 
-    const yearOf..........16 - age;
-    return ...
-  }
